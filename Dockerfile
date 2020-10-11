3451e0d6e1eed8ce2364edd5ac6bc4357000263f
Josh Bakos <jzbakos@gmail.com>
2020-10-11 18:23:00 -0400

    setup base layout

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
-FROM ubuntu:18.04
+FROM python:3
 
 WORKDIR /usr/src/app
 
 COPY . .
 
-RUN apt-get update -y && apt-get install -y libsm6 libxext6 && apt-get install python3 -y && apt-get install python3-pip -y 
+RUN pip install -r requirements.txt
 
-RUN pip3 install -r requirements.txt
-
-CMD ["python3", "app.py"]
+CMD ["python", "app.py"]
 
 
 
--- app.py
+++ app.py
@@ -10,6 +10,30 @@ def index():
     return render_template("public/index.html")
 
 
+# Menu route
+@app.route("/menu")
+def menu():
+    return render_template("public/menu.html")
+
+
+# Order route
+@app.route("/order")
+def order():
+    return render_template("public/order.html")
+
+
+# Register route
+@app.route("/register")
+def register():
+    return render_template("public/register.html")
+
+
+# Login route
+@app.route("/login")
+def login():
+    return render_template("public/login.html")
+
+
 # port = os.environ["PORT"]
 port = 5000
 app.run(host="0.0.0.0", port=port)
