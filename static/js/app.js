ee9b5bfb0aef18be74685403872b43f41a95564e
Josh Bakos <jzbakos@gmail.com>
2020-11-26 13:10:33 -0500

    Updated dashboard.

--- static/js/app.js
+++ static/js/app.js
@@ -15,29 +15,4 @@ function alertMessage(type, message)
 
     msgSuccess.innerHTML = message;
     setTimeout(function() {msgSuccess.innerHTML = ''; msgSuccess.className = '';}, 2000);
-    
-
-    // Check if alert is visible
-    // if (msgSuccess.style.display == 'none')
-    // {
-    //     msgSuccess.style.display = 'block';
-    //     msgSuccess.innerHTML = message;
-    //     setTimeout(function() {msgSuccess.style.display = 'none'}, 3000);
-    // }
-    // else {
-    //     msgSuccess.innerHTML = message;
-    // }
-}
-
-// Remove an item from the order
-function removeOrderItem()
-{
-     
-}
-
-
-// Add an item to the order
-function addOrderItem()
-{
-    
 }
